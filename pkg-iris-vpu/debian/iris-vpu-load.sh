a74162d431973136d0b16c757fa5b1a194773ff7
Renjiang Han <renjiang@qti.qualcomm.com>
2026-07-30 09:52:07 +0530

    pkg-iris-vpu: add license header to iris-vpu-load script
    Add license to iris-vpu-load.sh for the license checker.
    
    Signed-off-by: Renjiang Han <renjiang@qti.qualcomm.com>

--- pkg-iris-vpu/debian/iris-vpu-load.sh
+++ pkg-iris-vpu/debian/iris-vpu-load.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+# SPDX-License-Identifier: GPL-2.0-only
+# Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+
 # Called by iris-vpu-dkms.service on every boot.
 # Load iris_vpu module, handling potential errors gracefully.
 lsmod | grep -q iris_vpu || modprobe iris_vpu
