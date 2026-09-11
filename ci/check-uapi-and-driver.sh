b237b736e0dcb5b45b17ff289d81f61c3c8b1708
renjiang-qti <renjiang@qti.qualcomm.com>
2026-09-11 16:00:26 +0800

    Update check-uapi-and-driver.sh
    Signed-off-by: renjiang-qti <renjiang@qti.qualcomm.com>

--- ci/check-uapi-and-driver.sh
+++ ci/check-uapi-and-driver.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# SPDX-License-Identifier: BSD-3-Clause-Clear
+# SPDX-License-Identifier: GPL-2.0-only
 # Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
 
 set -euo pipefail
